bcca118495555ff95884ee9987a187575a6eaf73
HasanAshab <samertask@gmail.com>
2024-10-09 16:31:55 +0600

    backup

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -6,7 +6,7 @@ var profileData = null
 var levelTitles = null
 
 async function setTaskActivity() {
-  const taskActivityData = await apiFetch("challenges/activities").as(username)
+  taskActivityData = await apiFetch("challenges/activities").as(username)
   const {
     total,
     failed,
@@ -26,14 +26,11 @@ async function setTaskActivity() {
     const html = `<span class="data"><span class="name ${slug}" style="background-color:${light_color}">${name}</span> <span class="value"> x${challenge_count}</span></span> `
     taskCompleteDetails.innerHTML += html
   }
-
-
 }
-async function setActiveTasksData() {
-  const tasksData = await apiFetch("challenges").as(username)
-  const activeTaskCount = document.getElementById('active-task-count')
-  activeTaskCount.textContent = tasksData.meta.count
 
+function setActiveTasksData() {
+  const activeTaskCount = document.getElementById('active-task-count')
+  activeTaskCount.textContent = taskActivityData.data.total - (taskActivityData.data.completed.total + taskActivityData.data.failed)
 }
 
 function setProfileData() {
@@ -50,13 +47,10 @@ function setProfileData() {
   authorLevel.textContent = `${profileData.level} / ${nextTitleRequiredLevel}`
   xpProgressbar.querySelector(".xp-count").textContent = currentXp
   xpProgressbar.querySelector(".complete-bg").style.width = `${(currentXp / 1000) * 100}%`
-  setActiveTasksData()
-  setTaskActivity()
 }
 async function fetchProfileData() {
   profileData = await apiFetch(`users/${username}`).as(username)
   profileData = profileData.data
-
 }
 async function fetchLevelTitles() {
   levelTitles = await apiFetch("level-titles").as(username)
@@ -66,20 +60,24 @@ async function fetchLevelTitles() {
 
 
 async function setAllData(params) {
-  await fetchLevelTitles()
-  await fetchProfileData()
+  await Promise.all([
+    fetchProfileData(),
+    fetchLevelTitles(),
+    setTaskActivity(),
+  ])
   setProfileData()
+  setActiveTasksData()
 }
 
 function switchAccount() {
   if (username === "hasan")
     username = "hossain"
-  else username = " hasan"
+  else username = "hasan"
   localStorage.setItem("username",username)
   setAllData()
 }
 
-window.onload = ()=> {
+window.onload = () => {
   const userSelectFeild = document.getElementById("user-select-feild")
   userSelectFeild.onchange = switchAccount
   userSelectFeild.selectedIndex = username === "hasan" ? 0 : 1
