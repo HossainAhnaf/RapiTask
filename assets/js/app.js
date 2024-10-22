da9e6bb6c26913f076d41759c0bfc95dca7a3b64
HasanAshab <samertask@gmail.com>
2024-10-22 17:04:39 +0600

    backup

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,12 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js')
     .then((registration) => {
         const remainingTasksCount = 3
-        registration.showNotification('Task Reminder', {
-            body: `You have ${remainingTasksCount} tasks remaining for the day.`,
-            icon: '/assets/img/robot.png',
-            badge: '/assets/img/robot.png',
-            requireInteraction: true, // Keeps the notification visible until user interacts
-          });
+        // registration.showNotification('Task Reminder', {
+//             body: `You have ${remainingTasksCount} tasks remaining for the day.`,
+//             icon: '/assets/img/robot.png',
+//             badge: '/assets/img/robot.png',
+//             requireInteraction: true, // Keeps the notification visible until user interacts
+//           });
     })
     .catch((error) => {
       console.error('Service Worker registration failed:', error);
